eb769944805d56d575d0bf7f35d26bd28e8ddbe1
Andrew Dean <oneandydean@hotmail.com>
2020-10-02 22:17:33 +0100

    Add code for handling 'SWAP BET' emails

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ IMAP_PORT = 993
 
 SUBJECT_REGEX = r"PROFORM (?P<type>NEW\-SELECTION|NON\-RUNNER|SWAP BET) \((?P<horse>[a-zA-Z ]+)\-(?P<time>[0-9]{2}\:[0-9]{2})\-(?P<course>[a-zA-Z ]+)\)"
 BODY_REGEX = r"NREP System: +(?P<system>JR\-TN2|JR\-DTR|JR\-MR3\.2|JR - LT6R)"
+SWAP_BET_BODY_REGEX = r"NREP TO +NEW System\: (?P<new_system>JR\-TN2|JR\-DTR|JR\-MR3\.2|JR - LT6R), FROM +OLD System\: (?P<old_system>JR\-TN2|JR\-DTR|JR\-MR3\.2|JR - LT6R)"
 
 mail = imaplib.IMAP4_SSL(IMAP_ADDRESS)
 mail.login(EMAIL_ADDRESS, PASSWORD)
@@ -31,30 +32,41 @@ def retrieve_emails():
 def notify_from_email(subject, body):
     """Send a Discord notification about the information in the given email."""
     subject_match = re.match(SUBJECT_REGEX, subject)
-
-    body_match = re.match(BODY_REGEX, body)
-
     notification_type = subject_match["type"]
-    system = body_match["system"]
     horse = subject_match["horse"]
     course = subject_match["course"]
     time = subject_match["time"]
 
-    if system == "JR-TN2":
-        system = "TN2"
-    elif system == "JR-DTR":
-        system = "DTR"
-    elif system == "JR-MR3":
-        system = "MR3"
-    elif system == "JR - LT6R":
-        system = "LT6R"
+    if notification_type == "SWAP BET":
+        body_match = re.match(SWAP_BET_BODY_REGEX, body)
+        old_system = tidied(body_match["old_system"])
+        new_system = tidied(body_match["new_system"])
+        notify(f"Swap bet ({old_system} -> {new_system}): {horse} ({time} {course})")
+        return
+
+    body_match = re.match(BODY_REGEX, body)
+    system = tidied(body_match["system"])
 
     if notification_type == "NEW-SELECTION":
-        notify(f"<@203581825451425792> New selection ({system}): {horse} ({time} {course})")
+        notify(f"<@203581825451425792> New selection ({system}): **{horse}** ({time} {course})")
     elif notification_type == "NON-RUNNER":
-        notify(f"Non-runner: ({system}): {horse} ({time} {course})")
+        notify(f"Non-runner ({system}): {horse} ({time} {course})")
     else:
         notify(f"<@203581825451425792> Unknown email format! Send help.")
+
+
+def tidied(system):
+    """Return the tidy version of the given system name."""
+    if system == "JR-TN2":
+        return "TN2"
+    elif system == "JR-DTR":
+        return "DTR"
+    elif system == "JR-MR3":
+        return "MR3"
+    elif system == "JR - LT6R":
+        return "LT6R"
+    else:
+        raise ValueError(f"Unknown system: '{system}'.")
 
 
 def loop():
