13dca9ea3de2aa89b5fc24cb6aa4873734df84be
Andrew Dean <oneandydean@hotmail.com>
2020-10-07 16:57:31 +0100

    Remove broken zipping from build_lambda.sh and request it's done manually by the user... for now

--- build_lambda.sh
+++ build_lambda.sh
@@ -1,5 +1,6 @@
+rm -rf lambda-build
 mkdir lambda-build
 cp lambda_function.py main.py notify.py credentials.yml lambda-build
 cd lambda-build
 pip install -r ../requirements.txt -t ./
-zip -r ../lambda-build.zip
+echo Please zip up the 'lambda-build' folder and upload it to AWS Lambda.
